2120b961d5565e0ca779bab7c6569ee2a99a8a00
Luca Scibetta s303976 <s303976@studenti.polito.it>
2022-10-20 15:40:44 +0200

    Added frontend call to APIs for new ticket request

--- code/frontend/src/services/api.js
+++ code/frontend/src/services/api.js
@@ -20,6 +20,22 @@ const api = {
                 .catch(err => reject(err))
         })
     },
+
+    getServiceList: () => {
+        return new Promise((resolve, reject) => {
+            axios.get(`${SERVER_URL}/services`)
+                .then(res => resolve(res.data))
+                .catch(err => reject(err));
+        })
+    },
+
+    postNewTicket: (serviceId) => {
+        return new Promise((resolve, reject) => {
+            axios.post(`${SERVER_URL}/tickets`, {serviceId: serviceId})
+                .then(res => resolve(res.data))
+                .catch(err => reject(err));
+        })
+    }
 };
 
 export default api;
